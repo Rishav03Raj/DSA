7dbe22c92011ea92c6f4c16d0ef2bff4a6fdf0aa
Rishav Raj <127576807+Rishav03Raj@users.noreply.github.com>
2025-06-25 23:19:18 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (76.55%) - LeetSync

---
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int rob1(vector<int>& nums) {
+        int n = nums.size();
+        int prev2 = 0;
+        int prev= nums[0] ;
+        int curr = 0;
+        for(int i=1;i<n;i++){
+            int pick = nums[i];
+            if(i>1) pick+=prev2;
+            int notpick = 0+prev;
+            curr = max(pick,notpick);
+            prev2 = prev;
+            prev = curr;
+        }
+        return prev;
+    }
+
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if(n==1) return nums[0];
+        vector<int> v1,v2;
+        for(int i=0;i<n;i++){
+            if(i!=0) v1.push_back(nums[i]);
+            if(i!=n-1) v2.push_back(nums[i]);
+        }
+        return max(rob1(v1),rob1(v2));
+
+    }
+};
