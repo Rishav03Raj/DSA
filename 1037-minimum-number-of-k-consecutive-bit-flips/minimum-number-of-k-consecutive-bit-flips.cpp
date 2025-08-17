dc23242c83a123fa654cfd039fc9f02fa3a19fd6
Rishav Raj <127576807+Rishav03Raj@users.noreply.github.com>
2025-08-17 10:51:33 +0530

    Time: 23 ms (6.60%) | Memory: 113.9 MB (40.14%) - LeetSync

--- 1037-minimum-number-of-k-consecutive-bit-flips/minimum-number-of-k-consecutive-bit-flips.cpp
+++ 1037-minimum-number-of-k-consecutive-bit-flips/minimum-number-of-k-consecutive-bit-flips.cpp
@@ -2,20 +2,24 @@ class Solution {
 public:
     int minKBitFlips(vector<int>& nums, int k) {
         int n = nums.size();
-        vector<bool> isflip(n,false);
+        // vector<bool> isflip(n,false);
+        deque<int> dq;
         int flipcntfrompast = 0;
         int flip = 0;
         for(int i=0;i<n;i++){
-            if(i>=k && isflip[i-k]){
-                flipcntfrompast--;
+            if(i>=k){
+                flipcntfrompast-=dq.front();
+                dq.pop_front();
             }
             if(flipcntfrompast % 2 == nums[i]){ // tells to flip if cond satisfied
                 if(i+k>n){
                     return -1;
                 }
                 flipcntfrompast++;
-                isflip[i]=true;
+                dq.push_back(1);
                 flip++;
+            }else{
+                dq.push_back(0);
             }
         }
         return flip;
