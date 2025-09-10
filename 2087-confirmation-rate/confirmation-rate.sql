dc045df3ab9e447459ee985bb2436a000d16a5cd
Rishav Raj <127576807+Rishav03Raj@users.noreply.github.com>
2025-09-10 11:58:29 +0530

    Time: 585 ms (94.37%) | Memory: 0B (100.00%) - LeetSync

--- 2087-confirmation-rate/confirmation-rate.sql
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -1,10 +1,11 @@
 # Write your MySQL query statement below
-select s.user_id , round(coalesce(ans.cf/ans.tt,0),2)confirmation_rate from Signups s 
-left join 
+select s.user_id , round(coalesce(c.cnf/c.tt,0),2) confirmation_rate from 
+Signups s left join 
 (
 select user_id , 
-count(case when action='confirmed' then 1 end) as cf ,
-count(*) as tt from 
-Confirmations group by user_id 
-) as ans
-on s.user_id = ans.user_id;
+count(case when action='confirmed' then 1 end) as cnf,
+count(*)  as tt
+from Confirmations 
+group by user_id
+) c
+on s.user_id = c.user_id
