23d6feb5ba8bc9c262c3f3103c526106cccee066
Rishav Raj <127576807+Rishav03Raj@users.noreply.github.com>
2025-10-08 01:15:08 +0530

    Time: 126 ms (49.00%) | Memory: 114.6 MB (57.83%) - LeetSync

---
+++ 1612-avoid-flood-in-the-city/avoid-flood-in-the-city.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> avoidFlood(vector<int>& rains) {
+        int n = rains.size();
+        vector<int> ans(n,1);
+        unordered_map<int,int> mp;
+        set<int> st;
+
+        for(int i=0;i<n;i++){
+            int lake = rains[i];
+            if(lake == 0){
+                st.insert(i);
+            }else{
+                ans[i] = -1;
+                if(mp.count(lake)){
+                        auto it = st.lower_bound(mp[lake]);
+                        if(it==st.end()){
+                            return {};
+                        }
+                        int dl = *it;
+                        ans[dl] = lake;
+                        st.erase(it);
+                }
+            }
+            mp[lake]=i;
+        }
+        
+        return ans;
+    }
+};
